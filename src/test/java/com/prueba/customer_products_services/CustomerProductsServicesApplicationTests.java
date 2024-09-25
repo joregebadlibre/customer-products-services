ceb5c3770f75a23e4258e6a116dd222b62ff24b7
joregebadlibre <joregebadlibre@hotmail.com>
2024-09-24 23:36:18 -0500

    Init

---
+++ src/test/java/com/prueba/customer_products_services/CustomerProductsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.customer_products_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerProductsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
