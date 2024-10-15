1318327a392bc7b695392912e23b8c989ffaaca9
joregebadlibre <joregebadlibre@hotmail.com>
2024-10-15 13:06:59 -0500

    Funcionalidad MovimientoCuenta-Cuenta

--- src/main/java/com/prueba/customer_products_services/repository/CuentaRepository.java
+++ src/main/java/com/prueba/customer_products_services/repository/CuentaRepository.java
@@ -2,10 +2,13 @@ package com.prueba.customer_products_services.repository;
 
 import com.prueba.customer_products_services.repository.entity.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
-    Optional<Cuenta> findByNumeroCuenta(String numeroCuenta);
+    @Query("SELECT c FROM cuenta c WHERE c.numero_cuenta = :numeroCuenta")
+    Optional<Cuenta> findByNumeroCuenta(@Param("numeroCuenta") String numeroCuenta);
 }
